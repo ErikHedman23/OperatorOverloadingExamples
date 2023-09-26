22bdddf973632a358eb38630105d4ccf2336b6d5
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-09-26 15:50:54 -0400

    Created a box class and instantiated it in the main program

--- OperatorOverloadingExamples/Program.cs
+++ OperatorOverloadingExamples/Program.cs
@@ -1,5 +1,15 @@
-﻿//Operator Overloading 
+﻿
+
+//Operator Overloading 
 
 //This is done with the overload keyword followed by the operator being defined; operator+
 //Similar to any method, an overloaded operator has a return type and a parameter list.
 
+using OperatorOverloadingExamples;
+
+Box b1 = new Box(14, 3);
+Box b2 = new Box(5, 7);
+Box b3 = b1 + b2;
+
+Console.WriteLine($" The height of Box 3 is: {b3.Height}");
+Console.WriteLine($"The width of Box 3 is: { b3.Width}");
