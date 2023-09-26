d3abc69caf20631b08356d4e57c9052059b2e8eb
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-09-26 15:15:49 -0400

    Add project files.

---
+++ OperatorOverloadingExamples/Program.cs
@@ -0,0 +1,5 @@
+﻿//Operator Overloading 
+
+//This is done with the overload keyword followed by the operator being defined; operator+
+//Similar to any method, an overloaded operator has a return type and a parameter list.
+
